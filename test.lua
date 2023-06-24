786abc8302ba54eba06ef618cccdd11d1a1e71db
Isglassen <vilhelmgg@gmail.com>
2023-06-24 22:06:47 +0200

    formating...

--- test.lua
+++ test.lua
@@ -13,24 +13,24 @@ end
 
 local function main()
   term.clear()
-  
+
   local function updateFunction(done, total, step, steps)
     term.clear()
     term.setCursorPos(1, 1)
     termFns.DetailedProgress(term, done, total, step, steps, colors.lime, colors.gray)
   end
-  
+
   items:refreshAll(updateFunction)
-  
+
   sleep(5)
-  
+
   items:refreshChest("minecraft:chest_5", updateFunction)
-  
+
   sleep(5)
-  
+
   items:pullItems(updateFunction, "minecraft:chest_6", "minecraft:chest_1", 1)
   items:pullItems(updateFunction, "minecraft:chest_6", "minecraft:chest_1", 2)
-  
+
   fileFns.writeData("out.log", { items = items.items, chests = items.chests, config = items.config })
 end
 
