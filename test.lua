344860beaae1563fd7bac0802cbaa36501f85623
Isglassen <vilhelmgg@gmail.com>
2023-06-24 22:06:34 +0200

    Testing even more stuff

--- test.lua
+++ test.lua
@@ -5,23 +5,33 @@ local config = fileFns.readData("storage_config.txt")
 
 local items = require("StorageData.items")(config)
 
-term.clear()
-
-local function updateFunction(done, total, step, steps)
+local function watchTerminate()
+  os.pullEventRaw("terminate")
   term.clear()
   term.setCursorPos(1, 1)
-  termFns.DetailedProgress(term, done, total, step, steps, colors.lime, colors.gray)
 end
 
-items:refreshAll(updateFunction)
+local function main()
+  term.clear()
+  
+  local function updateFunction(done, total, step, steps)
+    term.clear()
+    term.setCursorPos(1, 1)
+    termFns.DetailedProgress(term, done, total, step, steps, colors.lime, colors.gray)
+  end
+  
+  items:refreshAll(updateFunction)
+  
+  sleep(5)
+  
+  items:refreshChest("minecraft:chest_5", updateFunction)
+  
+  sleep(5)
+  
+  items:pullItems(updateFunction, "minecraft:chest_6", "minecraft:chest_1", 1)
+  items:pullItems(updateFunction, "minecraft:chest_6", "minecraft:chest_1", 2)
+  
+  fileFns.writeData("out.log", { items = items.items, chests = items.chests, config = items.config })
+end
 
-sleep(5)
-
-items:refreshChest("minecraft:chest_5", updateFunction)
-
-sleep(5)
-
-items:pullItems(updateFunction, "minecraft:chest_6", "minecraft:chest_1", 1)
-items:pullItems(updateFunction, "minecraft:chest_6", "minecraft:chest_1", 2)
-
-fileFns.writeData("out.log", { items = items.items, chests = items.chests, config = items.config })
+parallel.waitForAny(watchTerminate, main)
